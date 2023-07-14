cf812899401621a831d3d2ca6b92af193cba1d8e
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2023-07-14 20:36:02 -0300

    requesito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,17 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    dictTxt = {}
+    quotes = txt_importer(path_file)
+    dictTxt["nome_do_arquivo"] = path_file
+    dictTxt["qtd_linhas"] = len(quotes)
+    dictTxt["linhas_do_arquivo"] = quotes
+    for itens in instance.queue:
+        if itens["nome_do_arquivo"] == path_file:
+            return None
+    instance.enqueue(dictTxt)
+    print(instance.queue)
 
 
 def remove(instance):
