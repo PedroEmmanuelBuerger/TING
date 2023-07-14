b0632ebef6f0037a3217be7895d2a23169a5c467
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2023-07-14 20:01:56 -0300

    requesito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file.endswith(".txt"):
+        try:
+            with open(path_file, "r") as file:
+                allLinesList = []
+                allLines = file.readlines()
+                for line in allLines:
+                    line = line.replace("\n", "")
+                    allLinesList.append(line)
+                return allLinesList
+        except FileNotFoundError:
+            print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+    else:
+        return print("Formato inválido", file=sys.stderr)
