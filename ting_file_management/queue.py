22368687c3a4fedc11114ca778d9c7ee0fc1ab34
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2023-07-14 19:32:04 -0300

    requesito 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,21 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.queue = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.queue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if not len(self.queue) == 0:
+            return self.queue.pop(0)
+        else:
+            return None
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0 or index >= len(self.queue):
+            raise IndexError("Índice Inválido ou Inexistente")
+        return self.queue[index]
