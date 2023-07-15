28ea7960327d370403b9ba3e21e3f509630015b4
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2023-07-14 21:23:13 -0300

    requesito 6

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,15 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    priority_queue = PriorityQueue()
+    priority_queue.enqueue({"qtd_linhas": 7})
+    priority_queue.enqueue({"qtd_linhas": 6})
+    priority_queue.enqueue({"qtd_linhas": 3})
+    priority_queue.enqueue({"qtd_linhas": 2})
+    assert priority_queue.dequeue() == {"qtd_linhas": 3}
+    assert priority_queue.search(0) == {"qtd_linhas": 2}
+    assert priority_queue.search(1) == {"qtd_linhas": 7}
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        priority_queue.search(4)
