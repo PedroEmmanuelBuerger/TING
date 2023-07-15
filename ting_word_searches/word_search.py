31d9458b14debac7999fb230efd9d63df3045858
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2023-07-15 00:10:41 -0300

    req 8 finalizando projeto

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,3 @@
-from ting_file_management.queue import Queue
-
-
 def exists_word(word, instance):
     itens_in_instance = []
     for itens in instance.queue:
@@ -19,7 +16,20 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    itens_in_instance = []
+    for itens in instance.queue:
+        actual_lines = []
+        for quote in itens["linhas_do_arquivo"]:
+            if word.lower() in quote.lower():
+                line = search_quote_line(quote, itens)
+                actual_lines.append({"linha": line, "conteudo": quote})
+        if len(actual_lines) > 0:
+            actual_dict = {}
+            actual_dict["palavra"] = word
+            actual_dict["arquivo"] = itens["nome_do_arquivo"]
+            actual_dict["ocorrencias"] = format_lines_with_word(actual_lines)
+            itens_in_instance.append(actual_dict)
+    return itens_in_instance
 
 
 def search_quote_line(quote, actualItem):
@@ -37,27 +47,11 @@ def format_lines(lines):
     return list_lines
 
 
-q1 = Queue()
-q1.enqueue(
-    {
-        "nome_do_arquivo": "arquivo1.txt",
-        "qtd_linhas": 5,
-        "linhas_do_arquivo": [
-            "Acima de tudo,",
-            "é fundamental ressaltar que a adoção de políticas nos obriga",
-            "à análise do levantamento das variáveis envolvidas.",
-        ],
-    }
-)
-
-q1.enqueue(
-    {
-        "nome_do_arquivo": "arquivo2.txt",
-        "qtd_linhas": 2,
-        "linhas_do_arquivo": [
-            "análise",
-            "de",
-        ],
-    }
-)
-print(exists_word("de", q1))
+def format_lines_with_word(lines):
+    list_lines = []
+    for line in lines:
+        dict_lines = {}
+        dict_lines["linha"] = line["linha"]
+        dict_lines["conteudo"] = line["conteudo"]
+        list_lines.append(dict_lines)
+    return list_lines
